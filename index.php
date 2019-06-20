f7feabd6aded4e94bca64f84e48da23b093d5504
Hector <32627138+lewdxample@users.noreply.github.com>
2019-06-20 15:41:43 +0700

    a

--- index.php
+++ index.php
@@ -23,6 +23,8 @@
     <script src="index.js"></script>
 </head>
 <body>
+        <button onclick="cdStart()">Start</button>
+        <p id="demo" class="countdown-live" style="text-align:right; font-size: 46px; margin-top: 0px; margin-right: 20px"><b>Time Limit: 7 m 0 s</b></p>
     <div class="container">
         <div class="header">
             <img class="s1" src="res/star.png" width="100px" height="100px" alt="star">
@@ -99,6 +101,38 @@
                             }
                         }
                 }
+
+                function cdStart(){
+                    // Set the date we're counting down to
+                    var countDownDate = new Date(Date.now() + 420000).getTime();
+
+                    // Update the count down every 1 second
+                    var x = setInterval(function() {
+
+                    // Get todays date and time
+                    var now = new Date().getTime();
+
+                    // Find the distance between now and the count down date
+                    var distance = countDownDate - now;
+
+                    // Time calculations for days, hours, minutes and seconds
+                    // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                    // var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+                    // Output the result in an element with id="demo"
+                    document.getElementById("demo").innerHTML = "<b>Time left : "+minutes + " m " + seconds + " s</b>";
+
+                    // If the count down is over, write some text 
+                    if (distance < 0) {
+                        clearInterval(x);
+                        document.getElementById("demo").innerHTML = '<b>TIME UP!</b>';
+                    }
+                    }, 1000);
+                }
+
+                
             </script>
 </body>
 </html>
